5f7f2db20a55bcc4f9401bf1861c9e215e861278
Catherine Smiley <catherinesmiley@gmail.com>
2020-02-01 22:50:51 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,17 +1,17 @@
-require 'pry'
+# require 'pry'
 
-class Backer 
+# class Backer 
   
-  attr_reader :backed_projects, :name
+#   attr_reader :backed_projects, :name
   
-  def initialize(name)
-    @name = name
-    @backed_projects = []
-  end
+#   def initialize(name)
+#     @name = name
+#     @backed_projects = []
+#   end
   
-  def back_project(project)
-    @backed_projects << project 
-    project.add_backer(self) unless project.backers.include?(self)
-  end 
+#   def back_project(project)
+#     @backed_projects << project 
+#     project.add_backer(self) unless project.backers.include?(self)
+#   end 
   
-end
+# end
